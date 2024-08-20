fefb73742dc5170204d382892aa9e1657e90c046
elkadourimostapha <elkadourimostapha97@gmail.com>
2024-08-20 10:24:29 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
